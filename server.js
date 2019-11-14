7ceb9fe02108362e9ad8ccc2125afdfe38290531
dashzou <fxfddfno@gmail.com>
2019-11-15 00:07:18 +0200

    connect to database and add some color to our console

--- server.js
+++ server.js
@@ -1,20 +1,37 @@
 const express = require('express');
 const dotEnv = require('dotenv');
-
-// Route files
-const bootcamps = require('./routes/bootcamps');
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotEnv.config({
   path: './config/dev.env'
 });
 
+// connect to database
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`);
+const server = app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold);
+});
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+
+  // close server
+  server.close(() => process.exit(1));
 });
