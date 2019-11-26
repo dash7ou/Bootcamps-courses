0ba389a53b3eefefb0287b4f1a6f12e22dc8ff88
dash zou <fxfddfno@gmail.com>
2019-11-26 23:00:53 +0200

    admin create user

--- controllers/user.js
+++ controllers/user.js
@@ -33,3 +33,24 @@ exports.getUsers = asyncHandler((req, res, next) => {
         data: user
     })
 });
+
+/**
+ *   @desc    create user
+ *   @route   POST /api/v1/auth/users
+ *   @access  Private/Admin
+ */
+
+exports.createUser = asyncHandler((req, res, next) => {
+    const {
+        body
+    } = req;
+
+    const user = await User.create({
+        ...body
+    });
+
+    res.status(200).send({
+        success: true,
+        dat: user
+    })
+});
