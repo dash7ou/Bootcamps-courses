aee95893ece9d7ad98cd560911a3de9e5915ecdf
dash zou <fxfddfno@gmail.com>
2019-11-27 21:11:32 +0200

    calc average rating bootcamps

--- models/Review.js
+++ models/Review.js
@@ -41,4 +41,40 @@ reviewSchema.index({
 });
 
 
+
+// Static method to get avg of review rating
+reviewSchema.statics.getAverageRating = async function (bootcampId) {
+    const obj = await this.aggregate([{
+            $match: {
+                bootcamp: bootcampId
+            }
+        },
+        {
+            $group: {
+                _id: '$bootcamp',
+                averageRating: {
+                    $avg: '$rating'
+                }
+            }
+        }
+    ]);
+    try {
+        await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+            averageRating: obj[0].averageRating
+        });
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+// Call getAverageCost after save
+reviewSchema.post('save', function () {
+    this.constructor.getAverageRating(this.bootcamp);
+});
+
+// Call getAverageCost before remove
+reviewSchema.pre('remove', function () {
+    this.constructor.getAverageRating(this.bootcamp);
+});
+
 module.exports = mongoose.model("Review", reviewSchema);
--- models/course.js
+++ models/course.js
@@ -1,61 +1,61 @@
 const mongoose = require('mongoose');
 
-const courseSchema = new mongoose.Schema(
-  {
-    title: {
-      type: String,
-      trim: true,
-      required: [true, 'Please add a course title']
-    },
-    description: {
-      type: String,
-      required: [true, 'Please add a description']
-    },
-    weeks: {
-      type: String,
-      required: [true, 'Please add number of weeks']
-    },
-    tuition: {
-      type: Number,
-      required: [true, 'Please add a tuition']
-    },
-    minimumSkill: {
-      type: String,
-      required: [true, 'Please add a minimum skill'],
-      enum: ['beginner', 'intermediate', 'advanced']
-    },
-    scholarshipAvailable: {
-      type: Boolean,
-      default: false
-    },
-    bootcamp: {
-      type: mongoose.Schema.ObjectId,
-      ref: 'Bootcamp',
-      required: true
-    },
-    user: {
-      type: mongoose.Schema.ObjectId,
-      ref: 'User',
-      required: true
-    }
+const courseSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    trim: true,
+    required: [true, 'Please add a course title']
   },
-  {
-    timestamps: true
+  description: {
+    type: String,
+    required: [true, 'Please add a description']
+  },
+  weeks: {
+    type: String,
+    required: [true, 'Please add number of weeks']
+  },
+  tuition: {
+    type: Number,
+    required: [true, 'Please add a tuition']
+  },
+  minimumSkill: {
+    type: String,
+    required: [true, 'Please add a minimum skill'],
+    enum: ['beginner', 'intermediate', 'advanced']
+  },
+  scholarshipAvailable: {
+    type: Boolean,
+    default: false
+  },
+  bootcamp: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Bootcamp',
+    required: true
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: true
   }
-);
+}, {
+  timestamps: true
+});
 
 // Static method to get avg of course tuitions
-courseSchema.statics.getAverageCost = async function(bootcampId) {
+courseSchema.statics.getAverageCost = async function (bootcampId) {
   console.log('Calculating avg cost...'.blue);
 
-  const obj = await this.aggregate([
-    {
-      $match: { bootcamp: bootcampId }
+  const obj = await this.aggregate([{
+      $match: {
+        bootcamp: bootcampId
+      }
     },
     {
       $group: {
         _id: '$bootcamp',
-        averageCost: { $avg: '$tuition' }
+        averageCost: {
+          $avg: '$tuition'
+        }
       }
     }
   ]);
@@ -69,12 +69,12 @@ courseSchema.statics.getAverageCost = async function(bootcampId) {
 };
 
 // Call getAverageCost after save
-courseSchema.post('save', function() {
+courseSchema.post('save', function () {
   this.constructor.getAverageCost(this.bootcamp);
 });
 
 // Call getAverageCost before remove
-courseSchema.pre('remove', function() {
+courseSchema.pre('remove', function () {
   this.constructor.getAverageCost(this.bootcamp);
 });
 
